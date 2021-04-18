f4046c0f46a3f07dd6edffcac0f591a73f6cbac9
DecGernon <declangernon.dg@gmail.com>
2021-04-18 20:10:45 +0100

    'Second Commit'

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pandas as pd
-BTC = pd.read_csv('Binance_BTCUSD.csv', parse_dates=['date'], index_col='date')
-print(BTC.head())
-ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date')
-print(ETH.head())
-DJI = pd.read_csv('DJI_HistoricalPrices.csv', parse_dates=['Date'])
-print(DJI.head())
-SP500 = pd.read_csv('SP500_HistoricalPrices.csv', parse_dates=['Date'])
-print(SP500.head())
+import matplotlib.pyplot as plt
 
-data = BTC['close'].describe()
-print(data)
+BTC = pd.read_csv('Binance_BTCUSD.csv', parse_dates=['date'], index_col='date')
+BTC = BTC.sort_index(ascending=True)
+BTC_Close = BTC['close']
+BTC_Close = pd.DataFrame(BTC_Close)
+
+ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date')
+ETH = ETH.sort_index(ascending=True)
+ETH_Close = ETH['close']
+ETH_Close = pd.DataFrame(ETH_Close)
+
+fig, ax = plt.subplots()
+ax.plot(BTC_Close, label='BTC')
+ax.plot(ETH_Close, label='ETH')
+ax.set_xlabel('Date')
+ax.set_ylabel('USD')
+ax.legend()
+plt.show()
+
+
+DJI = pd.read_csv('DJI_HistoricalPrices.csv', parse_dates=['Date'])
+
+SP500 = pd.read_csv('SP500_HistoricalPrices.csv', parse_dates=['Date'])
+
