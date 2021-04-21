06d03177a0ea16c0044f51d5709733ca89c53e78
DecGernon <declangernon.dg@gmail.com>
2021-04-21 20:16:29 +0100

    'Fifth Commit'

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ print(BTC.info())
 BTC = BTC.sort_index(ascending=True)
 BTC_Close = BTC['close']
 BTC_Close = pd.DataFrame(BTC_Close)
+print(BTC_Close.describe())
+returns = BTC_Close.pct_change()*100
+print(returns.head())
 
-ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date')
+ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date')  #look at again
 print(ETH.info())
 ETH = ETH.sort_index(ascending=True)
 ETH_Close = ETH['close']
 ETH_Close = pd.DataFrame(ETH_Close)
+print(ETH_Close.describe())
 
 plt.style.use('bmh')
 fig, ax = plt.subplots(2, 1)
@@ -31,6 +35,7 @@ DJI = DJI.sort_index(ascending=True)
 DJI_Close = DJI['Close']
 DJI_Close = pd.DataFrame(DJI_Close)
 print(DJI_Close.head())
+print(DJI_Close.describe())
 
 SP500 = pd.read_csv('SP500_HistoricalPrices.csv', parse_dates=['Date'], index_col='Date')
 print(SP500.info())
@@ -38,6 +43,7 @@ SP500 = SP500.sort_index(ascending=True)
 SP500_Close = SP500['Close']
 SP500_Close = pd.DataFrame(SP500_Close)
 print(SP500_Close.head())
+print(SP500_Close.describe())
 
 plt.style.use('bmh')
 fig, ax = plt.subplots(2, 1)
