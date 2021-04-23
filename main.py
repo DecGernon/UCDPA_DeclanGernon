0da22262f75031a3a46d6d9730cdf564f01a70a4
DecGernon <declangernon.dg@gmail.com>
2021-04-23 19:44:07 +0100

    '7th Commit'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 BTC = pd.read_csv('Binance_BTCUSD.csv', parse_dates=['date'], index_col='date')
 print(BTC.info())
@@ -9,9 +10,10 @@ BTC_Close = pd.DataFrame(BTC_Close)
 print(BTC_Close.describe())
 BTC_FirstPrice = BTC.close.iloc[0]
 BTC_Normalised = BTC.close.div(BTC_FirstPrice).mul(100)
+BTC_NormalisedDF = pd.DataFrame(BTC_Normalised)
+print(BTC_NormalisedDF.head())
 
 
-'#look at again#'
 ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date', low_memory=False)
 print(ETH.info())
 ETH = ETH.sort_index(ascending=True)
@@ -20,9 +22,17 @@ ETH_Close = pd.DataFrame(ETH_Close)
 print(ETH_Close.describe())
 ETH_FirstPrice = ETH.close.iloc[0]
 ETH_Normalised = ETH.close.div(ETH_FirstPrice).mul(100)
+ETH_NormalisedDF = pd.DataFrame(ETH_Normalised)
+print(ETH_NormalisedDF.head())
+print(ETH_NormalisedDF.info())
 
-DataNorm = pd.DataFrame(data= BTC_Normalised + ETH_Normalised)
-print(DataNorm.head())
+MergedCrypto = pd.merge_ordered(BTC_NormalisedDF, ETH_NormalisedDF, on='date')
+MergedCrypto = MergedCrypto.set_index('date')
+MergedCrypto.rename(columns={'close_x': 'BTC Normalised Daily Returns', 'close_y': 'ETH Normalised Daily Returns'},
+                    inplace=True)
+print(MergedCrypto.head())
+MergedCrypto.plot(title='Normalised Crypto Returns')
+plt.show()
 
 
 plt.style.use('bmh')
@@ -70,4 +80,3 @@ ax[1].set_ylabel('USD')
 ax[1].set_title('Dow Jones Index')
 plt.show()
 
-NormData = pd.concat([BTC_Normalised, ETH_Normalised])
