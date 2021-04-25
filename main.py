fffb9921ed293f36691cec6cf19ea22f574d4612
DecGernon <declangernon.dg@gmail.com>
2021-04-25 18:34:13 +0100

    '11th Commit'

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+"""import required libraries"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import numpy_financial as npf
 import seaborn as sns
+
+"""import, clean & sort data from CSV files"""
 
 BTC = pd.read_csv('Binance_BTCUSD.csv', parse_dates=['date'], index_col='date', na_values='n/a')
 BTC = BTC.dropna(axis=1)
 print(BTC.info())
 BTC = BTC.sort_index(ascending=True)
+
+"""Selecting column which contains closing prices and displaying summary statistics"""
+
 BTC_Close = BTC['close']
 BTC_Close = pd.DataFrame(BTC_Close)
 print(BTC_Close.describe())
+
+"""Normalising the Closing Price data for comparison purposes"""
+
 BTC_FirstPrice = BTC.close.iloc[0]
 BTC_Normalised = BTC.close.div(BTC_FirstPrice).mul(100)
 BTC_NormalisedDF = pd.DataFrame(BTC_Normalised)
 print(BTC_NormalisedDF.head())
+
+"""Repeat above process for next CSV file"""
 
 ETH = pd.read_csv('Binance_ETHUSD.csv', parse_dates=['date'], index_col='date', low_memory=False)
 print(ETH.info())
@@ -28,6 +39,8 @@ ETH_Normalised = ETH.close.div(ETH_FirstPrice).mul(100)
 ETH_NormalisedDF = pd.DataFrame(ETH_Normalised)
 print(ETH_NormalisedDF.head())
 print(ETH_NormalisedDF.info())
+
+"""Merge the above DataFrames and rename columns"""
 
 MergedCrypto = pd.merge_ordered(BTC_NormalisedDF, ETH_NormalisedDF, on='date')
 MergedCrypto = MergedCrypto.set_index('date')
@@ -51,13 +64,20 @@ ax[1].set_ylabel('USD')
 ax[1].set_title('ETH/USD')
 plt.show()
 
+"""import, clean & sort data from CSV files"""
+
 DJI = pd.read_csv('DJI_HistoricalPrices.csv', parse_dates=['date'], index_col='date')
 print(DJI.info())
 DJI = DJI.sort_index(ascending=True)
+
+"""Selecting column which contains closing prices and displaying summary statistics"""
+
 DJI_Close = DJI['close']
 DJI_Close = pd.DataFrame(DJI_Close)
 print(DJI_Close.head())
 print(DJI_Close.describe())
+
+"""User defined function to automate the normalising process"""
 
 
 def normalise(x):
@@ -73,17 +93,26 @@ DJI_NormalisedDF = normalise(DJI)
 print(DJI_NormalisedDF.info())
 print(DJI_NormalisedDF.head())
 
+"""import, clean & sort data from CSV files"""
+
 SP500 = pd.read_csv('SP500_HistoricalPrices.csv', parse_dates=['date'], index_col='date')
 print(SP500.info())
 SP500 = SP500.sort_index(ascending=True)
+
+"""Selecting column which contains closing prices and displaying summary statistics"""
+
 SP500_Close = SP500['close']
 SP500_Close = pd.DataFrame(SP500_Close)
 print(SP500_Close.head())
 print(SP500_Close.describe())
 
+"""Use user defined function"""
+
 SP500_NormalisedDF = normalise(SP500)
 print(SP500_NormalisedDF.info())
 print(SP500_NormalisedDF.head())
+
+"""Merge above DataFrame"""
 
 MergedIndices = pd.merge_ordered(DJI_NormalisedDF, SP500_NormalisedDF, on='date')
 MergedIndices = MergedIndices.set_index('date')
@@ -98,6 +127,7 @@ IndicesClose = IndicesClose.set_index('date')
 IndicesClose.rename(columns={'close_x': 'Dow Jones Close', 'close_y': 'SP500 Close'}, inplace=True)
 print(IndicesClose.head())
 
+"""Create merged data frames showing Closing Prices & Normalised Growth Rates for all of the assets data"""
 
 MergedDataFrames = pd.merge_ordered(MergedIndices, MergedCrypto, on='date')
 MergedDataFrames = MergedDataFrames.set_index('date')
@@ -110,6 +140,8 @@ MergedClose = MergedClose.set_index('date')
 print(MergedClose.head())
 MergedClose = MergedClose.dropna(axis=0)
 print(MergedClose.head())
+
+"""Calculate correlation between asset prices and display using heatmap """
 
 ClosePriceCorrelation = MergedClose.corr()
 print(ClosePriceCorrelation.head())
@@ -127,11 +159,18 @@ ax[1].set_ylabel('USD')
 ax[1].set_title('Dow Jones Index')
 plt.show()
 
+"""Show the future value of 10000 USD invested in each asset on 2017-08-17 until 2021-04-01"""
+
 EndDateGrowthRate = MergedDataFrames.loc['2021-04-01']
 print(EndDateGrowthRate)
-'#invest 10000 USD on 2017-08-17 until 2021-04-01#'
 FutureValue = 10000 * EndDateGrowthRate/100
+FutureValue = FutureValue.rename(index={'Dow Jones Normalised Growth Rate': 'Dow Jones Future Value',
+                                        'SP500 Normalised Growth Rate': 'SP500 Future Value',
+                                        'BTC Normalised Growth Rate': 'BTC Future Value',
+                                        'ETH Normalised Growth Rate': 'ETH Future Value'})
 print(FutureValue)
 
-DiscountedValue = np.pv(rate=0.02, nper=4, pmt=0, fv=FutureValue)
+"""Discount the future values assuming an inflation rate 2% and approx 4 year period"""
+
+DiscountedValue = npf.pv(rate=0.02, nper=4, pmt=0, fv=-FutureValue)
 print(DiscountedValue)
